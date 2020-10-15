0e259421aa78f7bcc4e07c8c1f8b806a2d83313b
MD-Nasir301 <nasir21b@gmail.com>
2020-10-16 00:50:10 +0600

    sidebar toggle function change

--- js/main.js
+++ js/main.js
@@ -2,25 +2,16 @@
   ("use strict");
 
   // sidebar toggle btn
-  $(window).resize(function () {
-    if ($(document).width() < 992) {
-      $("#sidebar-left-sec").css("left", "-351px");
-
-      $(".sidebar-toggle-btn").on("click", () => {
-        $("#sidebar-left-sec").css("left", "0px");
-        $(".close-btn").show();
-      });
-      $(".close-btn").on("click", () => {
-        $("#sidebar-left-sec").css("left", "-351px");
-        $(".close-btn").hide();
-      });
-    }
-
-    if ($(document).width() >= 992) {
+    $(".sidebar-toggle-btn").on("click", () => {
       $("#sidebar-left-sec").css("left", "0px");
-    }
+      $(".close-btn").show();
+    });
 
-  });
+    $(".close-btn").on("click", () => {
+      $("#sidebar-left-sec").css("left", "-351px");
+      $(".close-btn").hide();
+    });
+
 
 
   AOS.init({
