ba227cd88ff5af60471581bffbdc0d91d99342db
MD-Nasir301 <nasir21b@gmail.com>
2020-10-16 02:18:31 +0600

    final work done

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 (function ($) {
   ("use strict");
 
-  // sidebar toggle btn
-    $(".sidebar-toggle-btn").on("click", () => {
-      $("#sidebar-left-sec").css("left", "0px");
-      $(".close-btn").show();
-    });
-
-    $(".close-btn").on("click", () => {
-      $("#sidebar-left-sec").css("left", "-351px");
+  // sidebar open btn
+  $(".sidebar-open-btn").on("click", () => {
+    $("#sidebar-left-sec").css("left", "0px");
+    $(".close-btn").show();
+  });
+  // sidebar close btn
+  $(".close-btn").on("click", () => {
+    $("#sidebar-left-sec").css("left", "-330px");
+    $(".close-btn").hide();
+  });
+  // Sidebar hides by clicking on any sidebar element for less than 992px devices
+  if ($(window).width() < 992) {
+    $("#sidebar-left-sec").click(function () {
+      $("#sidebar-left-sec").css("left", "-341px");
       $(".close-btn").hide();
     });
+  }
 
 
-
+  // animation aos
   AOS.init({
     offset: 60,
     delay: 00,
-    duration: 1100, 
-    easing: 'ease', 
-    mirror: false, 
-    anchorPlacement: 'top-bottom', 
-  
+    duration: 1100,
+    easing: "ease",
+    mirror: false,
+    anchorPlacement: "top-bottom",
   });
-
-
 })(jQuery);
